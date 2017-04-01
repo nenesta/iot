2d8a0f56c85a98e120129860bb3c08fba7c6fd5a
nenesta <rene.solis.guzman@gmail.com>
2017-04-01 18:34:53 -0300

    Se avanza un poco

--- js/iot.js
+++ js/iot.js
@@ -2,27 +2,43 @@ $(document).ready(function(){
 	var offset;
 	var xPos;
 	var yPos;
+	var xPosOld;
+	var yPosOld;
 	var offsetBox;
 	var xPosBox;
 	var yPosBox;
 	var elemento;
+        
+//        $("#box").hover(function(){
+//        		console.log("Estoy en la posicion "+ $(this).position().top + ' - '+$(this).position().left);
+//        	});
+	$("#box").mouseenter(function(){
+    		console.log("El ratón está sobre el div negro");
+  	});
+ 
+	$("#box").mouseleave(function(){
+    		console.log("El ratón ya no está sobre el div negro");
+ 	});
+        
 	$(".cuadrado").draggable({
 		start: function(event, ui){
-			  offset = $(this).offset();
+			  offset = $(this).position();
+			  xPosOld = offset.left;
+			  yPosOld = offset.top;
 			  offsetBox = $('#box').position();
 			  xPosBox = offsetBox.left;
 			  yPosBox = offsetBox.top;
 			  
 		},	
 		stop: function(event, ui){
-			  elemento = $(this).clone();
 			  offset = $(this).position();
 			  xPos = offset.left;
 			  yPos = offset.top;
 			  if(xPos>xPosBox && yPos>yPosBox){
-				(elemento).css('position', 'absolute');
-				(elemento).css('background-color', '#ffff00');
-
+        			elemento = $(this).clone().draggable();
+				elemento.css('position', 'absolute');
+				elemento.css('background-color', '#ffff00');
+                                $(this).offset({ top: yPosOld, left: xPosOld });
 				$('#box').append(elemento);
 				console.log((elemento).attr('css'));
 			  }
