d69b7286d79ba768fbf4217df629426db3032eaa
nenesta <rene.solis.guzman@gmail.com>
2017-04-01 17:23:12 -0300

    Se limpia codigo

--- js/iot.js
+++ js/iot.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
-/*	  
-	$(".cuadrado").keypress(function(e, h){
-		console.log("estoy presionando sobre el cuadrado");
-	});	
-	$(".circulo").keypress(function(e, h){
-		console.log("estoy presionando sobre el circulo");
-	});	
-	$(".cuadrado").hover(function(){
-		console.log("*******estoy presionando sobre el cuadrado");
-	});	
-	$(".circulo").hover(function(){
-		console.log("*******estoy presionando sobre el circulo");
-	});	
-	
-*/	
 	var offset;
 	var xPos;
 	var yPos;
@@ -23,38 +8,23 @@ $(document).ready(function(){
 	var elemento;
 	$(".cuadrado").draggable({
 		start: function(event, ui){
-			  //console.log("CUAD: Se ha producido el evento start");
-			  //console.log(event);
-			  //console.log(ui);
-//			  elemento = $(this).clone();
-			  //console.log(elemento);
 			  offset = $(this).offset();
-			  xPos = offset.left;
-			  yPos = offset.top;
-			  //console.log(xPos + ", "+yPos);
 			  offsetBox = $('#box').position();
 			  xPosBox = offsetBox.left;
 			  yPosBox = offsetBox.top;
-			  //console.log(xPosBox + ", "+yPosBox);
 			  
 		},	
 		stop: function(event, ui){
 			  elemento = $(this).clone();
-			  //console.log("CUAD: Se ha producido el evento stop");
 			  offset = $(this).position();
 			  xPos = offset.left;
 			  yPos = offset.top;
-			  console.log(xPos + ", "+yPos);
-			  console.log(xPosBox + ", "+yPosBox);
 			  if(xPos>xPosBox && yPos>yPosBox){
 				(elemento).css('position', 'absolute');
-				//(elemento).draggable('destroy');
-				(elemento).css('position-left', xPos);
-				(elemento).css('position-top', yPos);
-				//(elemento).css('float', 'inherit');
+				(elemento).css('background-color', '#ffff00');
+
 				$('#box').append(elemento);
 				console.log((elemento).attr('css'));
-				//console.log($('#box').html());
 			  }
 		}	
 	});
