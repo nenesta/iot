41dc965128ec1194d298dca70a2707bd6cf5c210
nenesta <rene.solis.guzman@gmail.com>
2017-05-07 03:10:59 -0300

    Se agregan archivos para integrar iconBar

---
+++ index2.php
@@ -0,0 +1,61 @@
+<html>
+	<head>
+        <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="js/editable-table-master/mindmup-editabletable.js"></script>
+        <script type="text/javascript" src="js/iot.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+        <link type="text/css" href="js/jquery-ui-themes-1.12.1/themes/redmond/jquery-ui.css" rel="stylesheet" />
+        <link type="text/css" href="css/iot.css" rel="stylesheet" />
+
+        <link type="text/css" href="css/iconBar.css" rel="stylesheet" />
+        <link type="text/css" href="css/font-awesome.min.css" rel="stylesheet" />
+	</head>
+	<body>
+<!--		<div id="botonera" width="1000" height="110">
+		
+			<div id="cuadrado" class="cuadrado">
+			
+			<div id="cuadrado" class="cuadrado" width="100px" heigth="100px">
+			</div>
+		
+			<div id="circulo" class="circulo">
+			
+			<div id="circulo" class="circulo">
+			</div>
+		</div>-->
+		<div id="icon-bar" width="1000" height="110">
+<!--		
+			<div id="cuadrado" class="cuadrado">
+-->			
+                        <div width="150px" heigth="150px">
+                            <div id="cuadrado" class="cuadrado" width="100px" heigth="100px">
+                            </div>
+                        </div>
+<!--		
+			<div id="circulo" class="circulo">
+-->			
+			<div width="150px" heigth="150px">
+                            <div id="circulo" class="circulo">
+                            </div>
+			</div>
+		</div>
+
+<!--                <div class="icon-bar">
+                  <a class="active" href="#"><i class="fa fa-home"></i></a>
+                  <a href="#"><i class="fa fa-search"></i></a>
+
+                  <a href="#"><i class="fa fa-square cuadrado"></i></a>
+                  <a href="#"><i class="fa fa-circle circulo"></i></a>
+
+                  <a href="#"><i class="fa fa-square"></i></a>
+                  <a href="#"><i class="fa fa-circle"></i></a>
+                  <a href="#"><i class="fa fa-trash"></i></a>
+
+                  
+                  <a href="#"><i class="cuadrado"/></i></a>
+
+                </div>            -->
+                <div id="box">
+                </div>
+	</body>
+</html>
